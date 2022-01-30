3bfe68d79ed2a2ea316a25e01b39f77ff1c187eb
wahyubudii <79882296+wahyubudii@users.noreply.github.com>
2022-01-30 19:09:50 +0700

    currently clear but there's logic error to find index in javascript

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,8 @@ use App\Http\Controllers\IndexController;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::resource('/index', IndexController::class);
